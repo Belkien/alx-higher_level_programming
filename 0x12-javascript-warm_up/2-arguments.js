e2210f18f0d5009d5b260b81c3a1ffc91bf348ca
Belkien <belkienmasinde2@gmail.com>
2023-03-14 02:47:58 +0000

    updates 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const count = process.argv.length;
-if (count === 2) {
-  console.log('No argument');
-} else if (count === 3) {
-  console.log('Argument found');
+const argc = process.argv.length;
+
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
